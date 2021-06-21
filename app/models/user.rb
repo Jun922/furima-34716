26c4d15ee346505512fd926a5149d7a4be499f77
Jun922 <junjunchi620@gmail.com>
2021-06-21 23:43:22 +0900

    first_commit

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname,           presence: true
+  validates :email,              presence: true, uniqueness: true
+  validates :encrypted_password, presence: true, length: { minimum: 6 }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/} do
+    validates :family_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/} do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+  validates :birthday,           presence: true
+end
