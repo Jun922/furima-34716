8f720299187fbbb6817278e43d22c491ca583659
Jun922 <junjunchi620@gmail.com>
2021-06-24 12:26:00 +0900

    ユーザー管理と単体テスト修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,           presence: true
-  validates :email,              presence: true
-  validates :encrypted_password, presence: true, length: { minimum: 6 }
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一ー-龥々]\z/} do
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :birthday
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
     validates :family_name
     validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
     validates :family_name_kana
     validates :first_name_kana
   end
-  validates :birthday,           presence: true
+
+  with_options presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i} do
+    validates :password
+    validates :password_confirmation
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,85 +1,96 @@
 require 'rails_helper'
-RSpec.describe User, type: :model do
-  describe 'ユーザー新規登録' do
 
-    # 正常系
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  
+  describe 'ユーザー新規登録' do
+    #正常系
     context '新規登録できる時' do
-      it 'nicknameが存在すれば登録できる' do
+      it '全ての項目の入力が存在すれば登録できること' do
+        expect(@user).to be_valid
       end
+
       it 'emailが＠つきであれば登録できる' do
+        @user.email = 'test@test'
+        expect(@user).to be_valid
       end
+
       it 'passwordとpassword_confirmationが6文字以上半角英数字であれば登録できる' do
+        @user.password = 'aaa000'
+        @user.password_confirmation = 'aaa000'
+        expect(@user).to be_valid
       end
-      it 'family_naemとfirst_nameが全角であれば登録できる' do
+
+      it 'family_nameとfirst_nameが全角であれば登録できる' do
+        @user.family_name = 'てすと'
+        @user.first_name = 'てすと'
+        expect(@user).to be_valid
       end
-      it 'family_naem_kanaとfirst_name_kanaが全角カナであれば登録できる' do
-      end
-      it 'birthdayが存在すれば登録できる' do
+
+      it 'family_name_kanaとfirst_name_kanaが全角カナであれば登録できる' do
+        @user.family_name_kana = 'テスト'
+        @user.first_name_kana = 'テスト'
+        expect(@user).to be_valid
       end
     end
 
     #異常系
-    it 'nicknameが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.nickname = ''
-      user.valid?
-      expect(user.errors.full_messages).to include "Nickname can't be blank"
-    end
+    context '新規登録できない時' do
+      it 'nicknameが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Nickname can't be blank"
+      end
 
-    it 'emailが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.email = ''
-      user.valid?
-      expect(user.errors.full_messages).to include "Email can't be blank"
-    end
+      it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Email can't be blank"
+      end
 
-    it 'passwordが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.password = ''
-      user.valid?
-      expect(user.errors.full_messages).to include "Password can't be blank"
-    end
-    
-    it 'password_confirmationが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.password_confirmation = ''
-      user.valid?
-      expect(user.errors.full_messages).to include "Password confirmation doesn't match Password"
-    end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password can't be blank"
+      end
+        
+      it 'password_confirmationが空では登録できない' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+      end
 
-    it 'family_nameが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.family_name = ''
-      user.valid?
-      expect(user.errors.full_messages).to include "Family name is invalid"
-    end
-    
-    it 'first_nameが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.first_name = ''
-      user.valid?
-      expect(user.errors.full_messages).to include "First name is invalid"
-    end
+      it 'family_nameが空では登録できない' do
+        @user.family_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Family name is invalid"
+      end
+        
+      it 'first_nameが空では登録できない' do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "First name is invalid"
+      end
 
-    it 'family_name_kanaが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.family_name_kana = ''
-      user.valid?
-      expect(user.errors.full_messages).to include "Family name kana is invalid"
-    end
-    
-    it 'first_name_kanaが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.first_name_kana = ''
-      user.valid?
-      expect(user.errors.full_messages).to include "First name kana is invalid"
-    end
+      it 'family_name_kanaが空では登録できない' do
+        @user.family_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Family name kana is invalid"
+      end
+        
+      it 'first_name_kanaが空では登録できない' do
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "First name kana is invalid"
+      end
 
-    it 'birthdayが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.birthday = ''
-      user.valid?
-      expect(user.errors.full_messages).to include "Birthday can't be blank"
+      it 'birthdayが空では登録できない' do
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Birthday can't be blank"
+      end
     end
   end
 end
