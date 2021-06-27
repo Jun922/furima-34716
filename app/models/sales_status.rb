04406f0e0e22092a8dc64cf25c96e7bacab2a46b
Jun922 <junjunchi620@gmail.com>
2021-06-27 20:16:33 +0900

    商品出品ページを表示できるようにする

--- app/models/sales_status.rb
+++ app/models/sales_status.rb
@@ -1,4 +1,4 @@
-class SalesStatus < ApplicationRecord
+class SalesStatus < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '新品・未使用' },
@@ -12,4 +12,4 @@ class SalesStatus < ApplicationRecord
 
   include ActiveHash::Associations
   has_many :items
-  end
+end
