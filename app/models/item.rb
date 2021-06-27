b17833ba67280c407eb05fe518855b4cbe21190e
Jun922 <junjunchi620@gmail.com>
2021-06-27 15:30:55 +0900

    activehashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,19 @@
 class Item < ApplicationRecord
   belongs_to :user
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
   has_one_attached :image
 
   validates :image,                  presence: true
   validates :name,                   presence: true
   validates :info,                   presence: true
-  validates :category_id,            presence: true
-  validates :sales_status_id,        presence: true
-  validates :shipping_fee_status_id, presence: true
-  validates :prefecture_id,          presence: true
-  validates :scheduled_delivery_id,  presence: true
+  validates :category_id,            numericality: {other_than: 1}
+  validates :sales_status_id,        numericality: {other_than: 1}
+  validates :shipping_fee_status_id, numericality: {other_than: 1}
+  validates :prefecture_id,          numericality: {other_than: 1}
+  validates :scheduled_delivery_id,  numericality: {other_than: 1}
   validates :price,                  presence: true
 end
